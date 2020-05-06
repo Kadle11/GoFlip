3149f2690addee46220e65df24418e111fbae562
Kadle11 <vish.xsport@gmail.com>
2020-05-07 02:45:36 +0530

    Clean Exit

--- makeCartoon.go
+++ makeCartoon.go
@@ -11,15 +11,16 @@ func convComic(srcPath string) gocv.Mat {
 	srcRGB := gocv.IMRead(srcPath, gocv.IMReadColor)
 	srcEdit := srcRGB
 	
-	nDownSamples := 2
+	nDownSamples := 0
 	nBilateralFilters := 10
 
-	var startPt image.Point
 	tmpImg := gocv.NewMat()
-	
+	defer tmpImg.Close()
 	/*Downsample using Gaussian Pyramid*/
 
 	for i:=0; i < nDownSamples; i++ {
+		imgSize := srcEdit.Size()	
+		startPt := image.Pt(imgSize[0]/2, imgSize[1]/2)
 		gocv.PyrDown(srcEdit, &srcEdit, startPt, gocv.BorderDefault)
 	}
 
@@ -30,20 +31,25 @@ func convComic(srcPath string) gocv.Mat {
 	}
 
 	/*Upsample using Gaussian Pyramid*/
-
+	
 	for i:=0; i < nDownSamples; i++ {
-		gocv.PyrUp(srcEdit, &srcEdit, startPt, gocv.BorderDefault)
+		imgSize := srcEdit.Size()	
+		endPt := image.Pt(imgSize[0]*2, imgSize[1]*2)
+		gocv.PyrUp(srcEdit, &srcEdit, endPt, gocv.BorderDefault)
 	}
 
 	/*Covert to Grayscale and Apply Median Blur*/
 	srcGray := gocv.NewMat()
 	srcBlur := gocv.NewMat()
-
+	defer srcGray.Close()
+	defer srcBlur.Close()
+	
 	gocv.CvtColor(srcRGB, &srcGray, gocv.ColorRGBToGray) // 7
 	gocv.MedianBlur(srcGray, &srcBlur, 3)
 	
 	/*Detect and Enhance Edges*/
 	srcEdge := gocv.NewMat()
+	defer srcEdge.Close()
 	gocv.AdaptiveThreshold(srcBlur, &srcEdge, 255.0, gocv.AdaptiveThresholdMean, gocv.ThresholdBinary, 7, 2)
 	gocv.CvtColor(srcEdge, &srcEdge, gocv.ColorGrayToBGR)
 	
